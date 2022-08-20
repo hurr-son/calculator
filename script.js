16e14c1efceadebffadca731989189f34f866bc7
hurr-son <hprewett05@gmail.com>
2022-08-20 13:49:55 -0500

    operators display when pressed and store in to a variable the operate function is not functional when the button is pressed.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const digits = document.querySelectorAll(".digit");
+const operatorsBtns = document.querySelectorAll(".operatorBtn");
 const display = document.getElementById("display");
-const addBtn = document.getElementById("add");
-const subtractBtn = document.getElementById("subtract");
-const multiplyBtn = document.getElementById("multiply");
-const divideBtn = document.getElementById("divide");
-const equalsBtn = document.getElementById("equals");
+const addBtn = document.getElementById("add")
+const subtractBtn = document.getElementById("subtract")
+const multiplyBtn = document.getElementById("multiply")
+const divideBtn = document.getElementById("divide")
+const equalsBtn = document.getElementById("equals")
+const clearBtn = document.getElementById("clear");
+
+//I want to be able to store the selected operator so the 
+//operate function can call it when clicked.
+
 
 
 let calcLog = {value1: 0, value2: 0};
@@ -12,17 +18,18 @@ let operators = {
     '+': add,
     '-': subract,
     '*': multiply,
-    '/': divide
+    '/': divide,
 }
 
 
 
-const two = document.getElementById('2');
-const three = document.getElementById('3');
-const four = document.getElementById('4');
 
-
-
+operatorsBtns.forEach(button => {button.addEventListener('click',
+function() {
+    display.innerHTML = button.textContent
+    key =  button.textContent
+}
+)})
 
 
 digits.forEach(digit => {digit.addEventListener('click', function() {
@@ -34,20 +41,27 @@ digits.forEach(digit => {digit.addEventListener('click', function() {
     else if (calcLog.value1 !== 0 && calcLog.value2 === 0) {
         calcLog.value2 = parseInt(digit.innerHTML)
     }
-    else {
-        calcLog.value1 = parseInt(digit.innerHTML)
-        calcLog.value2 = 0
-    }   
 })})
 
-two.click()
-three.click()
-//four.click()
-console.table(calcLog)
+
 
 function operate(operator, a, b) {
-    return operator.call(this, a, b);
+    
+    display.innerHTML = operator.call(this, a, b);
 };
+let key = '';
+subtractBtn.click()
+let chosenOperator = operators[key]
+console.log(key)
+console.log(operators[key])
+
+equalsBtn.addEventListener('click', operate
+
+)
+
+console.log(operate(chosenOperator, 5, 1))
+
+
 
 
 function add(a, b) {
@@ -65,4 +79,5 @@ function multiply(a, b) {
 function divide(a, b) {
     return (a / b);
 };
+
 
